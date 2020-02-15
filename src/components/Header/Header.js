b060f248b59684599f6bc1fd6f87a768aecf6d05
Alex <alexg1990@users.noreply.github.com>
2020-02-15 19:06:44 +0100

    added header initial styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "./Header.css";
+
 const Header = function() {
   return (
-    <div>
-      <Link to="/">General</Link>
-      <Link to="/javascript">Javascript</Link>
-      <Link to="/react">React</Link>
-      <Link to="/typescript">Typescript</Link>
+    <div className="container">
+      <Link className="link" to="/">
+        General
+      </Link>
+      <Link className="link" to="/javascript">
+        Javascript
+      </Link>
+      <Link className="link" to="/react">
+        React
+      </Link>
+      <Link className="link" to="/typescript">
+        Typescript
+      </Link>
     </div>
   );
 };
